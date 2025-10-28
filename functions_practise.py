eede23ee125844baf5a6b1049c19967340558609
surag29 <surudev29@gmail.com>
2025-10-28 22:33:56 +0530

    Add various utility functions in functions_practise.py

--- functions_practise.py
+++ functions_practise.py
@@ -1,13 +1,14 @@
+#function to add 2 numbers
 def sum(a,b):
     return a+b
 print(sum(33,35))
 
-
+#function to understand parameters and arguments
 def pract(name,age=22):
     print(f"your  age is {age} and name is {name}")
 pract(name = "surag",age = 21)
 
-
+#function to check palindrome
 def palindrome_check(st):
     rev = ""
     for i in range(len(st)-1,-1,-1):
@@ -19,7 +20,7 @@ def palindrome_check(st):
 palindrome_check("surag")
 
 
-
+# function to find greatest number 
 def greatest(a,b,c):
     if a>=b and a>=c:
         print(f"{a} is greatest")
@@ -30,16 +31,16 @@ def greatest(a,b,c):
 
 greatest(4,8,5)
 
-
+# print in same line 
 for i in range(1,6):
     print("hello world",end = " ")
-
+#to print stars
 n =  int(input("enter the number "))
 for i in range(n,0,-1):
     print("*"*i)
 
 
-
+#fucntion to print sum of natural numbers
 def sum_of_natural_numbers(n):
     if n==1:
         return 1
@@ -47,6 +48,7 @@ def sum_of_natural_numbers(n):
         return n + sum_of_natural_numbers(n-1)
 print(sum_of_natural_numbers(5))
 
+#function to print stars
 def star(n):
     if n==0:
         return
@@ -55,12 +57,14 @@ def star(n):
         star(n-1)
 star(5)
 
+#function to convert inch to cm
 def inch_to_cm(inch):
     cm = inch*2.54
     return cm
 n = int(input("enter the inch value "))
 print(inch_to_cm(n))
 
+#function to print multiplication table 
 def multiplication_table(n):
     for i in range(1,11):
         print(f"{n} x {i} = {n*i}")
