5eba5770a66e366320c608f93cf7d5327670c227
surag29 <surudev29@gmail.com>
2025-11-02 23:20:30 +0530

    oops practise

---
+++ oops_concept_practise/oops_practise_problem1.py
@@ -0,0 +1,12 @@
+class programmer:
+    company =  "microsoft"
+
+    def __init__(self,name,salary,pin):
+        self.name = name
+        self.salary =  salary
+        self.pin =  pin
+p =  programmer("surag",90000,585103)
+print(p.name,p.salary,p.company,p.pin)
+r =  programmer("suru",90000,585103)
+print(r.name,r.salary,r.company,r.pin)
+    
